2f9eed00415ee62167e16815435bbc6d6c48537b
SMejia115 <s.mejia6@utp.edu.co>
2023-11-21 02:04:17 -0500

    Refactor bot vehicle initialization and update logic

--- botVehicle.py
+++ botVehicle.py
@@ -3,30 +3,36 @@ from pygame.sprite import Sprite
 
 class BotVehicle(Sprite):
 
-  def __init__(self, settings, screen, bot_image, pos_x, pos_y, direction):
+  def __init__(self, settings, screen, bot_image, direction, rail):
     super(BotVehicle, self).__init__()
 
     self.screen = screen
     self.settings = settings
     self.screen_rect = screen.get_rect()
-
+    self.direction = direction
     self.image = bot_image
     self.rect = self.image.get_rect()
     self.screen_rect = screen.get_rect()
+    self.rail = rail
+    self.rails = [265, 370, 480, 580]
+    self.rect.centerx= self.rails[self.rail] + self.rect.width/2
 
-    self.rect.centerx = self.screen_rect.centerx
-    self.rect.bottom = self.screen_rect.bottom
+    if self.direction == 1:
+      self.rect.bottom = 0
+      self.image = pygame.transform.flip(self.image, False, True)
+    else:
+      self.rect.bottom = self.screen_rect.bottom + self.rect.height
 
     self.center = float(self.rect.centerx)
     self.bottom = float(self.rect.bottom)
 
   def update(self, settings):
     if self.direction == 1:
-      self.center += settings.bot_speed_factor
+      self.bottom += (settings.bg_speed-1)
     else:
-      self.center -= settings.bot_speed_factor
+      self.bottom -= (settings.bg_speed-1)
 
-    self.rect.centerx = self.center
+    self.rect.bottom = self.bottom
 
   def draw(self):
     self.screen.blit(self.image, self.rect)
